285a1e25272d4f8b1c9a1da498f72545317255ae
sviridovairis@gmail.com <sviridovairis@gmail.com>
2024-12-18 14:44:26 +0300

    make configure services and configure app methods

--- AddressWebApi/Program.cs
+++ AddressWebApi/Program.cs
@@ -1,24 +1,41 @@
 using System.Net.Http;
 using System.Text.Json;
 using System.Text;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOpenApi();
+ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.MapOpenApi();
-}
+Configure(app);
 
-app.UseHttpsRedirection();
+app.MapGet("/api/getStandartAddress", () => "StandartAddress");
 
-//app.MapPost("/getStandartAddress", (...) =>
+app.Run();
+
+
+//https://localhost:44317/
+
+//app.MapGet("/getStandartAddress", (...) =>
 //{
 //    new HttpClient(), await httpClient.PostAsync(uri, addressListContent)
 //    return всю модель, потом из нее automapper преобразовать на объект с 5 полями
 //});
 
-app.Run();
+
+static void ConfigureServices(IServiceCollection services)
+{
+	services.AddOpenApi();
+}
+
+static void Configure(WebApplication app)
+{
+	if (app.Environment.IsDevelopment())
+	{
+		app.MapOpenApi();
+	}
+
+	app.UseHttpsRedirection();
+}
